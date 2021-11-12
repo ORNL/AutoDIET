b51deb05a74e0e183bceaaf759defd94354f3f1f
ssomnath <14300780+ssomnath@users.noreply.github.com>
2021-11-12 18:27:30 -0500

    Basic documentation

--- dfingest/cloud/cloud_provider.py
+++ dfingest/cloud/cloud_provider.py
@@ -1,7 +1,11 @@
 class CloudProvider(object):
+    """
+    Abstract class for a cloud provider capable of uploading a file and hosting
+    it such that it would be publicly available for embedding in web services
+    """
 
     def __init__(self, *args, **kwargs):
-        raise NotImplementedError('Constructor not overriden from base class')
+        raise NotImplementedError('Constructor not overridden from base class')
 
     def upload_public_file(self, local_path, dest_path):
         raise NotImplementedError('Function not implemented in child class')
